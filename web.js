afb2c97329fb147c3542166f72b3ce24d888723d
Eric Bakan <eric@ebakan.com>
2013-03-27 08:39:26 -0700

    testing things

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var team="254";
+  var ev="casd";
+  var baseurl="http://chiefdelphi.com/forums/frcspy.php?xml=2";
+  var cdurl=baseurl+"&teams="+team+"&events="+ev;
+  var refresh=10;
+  $.get(url,function(xml) {
+  var json = $.xml2json(xml);
+  console.log(json);
+  response.send(json);
+  });
 });
 
 var port = process.env.PORT || 5000;
