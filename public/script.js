b4540888d3542979f477d1fdb45a36e09beee379
Eric Bakan <eric@ebakan.com>
2013-03-27 11:01:40 -0700

    Things work

---
+++ public/script.js
@@ -0,0 +1,64 @@
+$(window).resize(function() {
+        //$("tbody").css("font-size","20px");
+    var size=parseInt($("tbody").css("font-size").substr(0,$("tbody").css("font-size").length-2));
+    while($(document).height()>$(window).height()) {
+        size=parseInt($("tbody").css("font-size").substr(0,$("tbody").css("font-size").length-2));
+        $("html").css("font-size",(size-1)+"px");
+        $("tbody").css("font-size",(size-1)+"px");
+        $("tbody").css("font-size",(size-1)+"px");
+        $("tbody").css("font-size",(size-1)+"px");
+        $(".maintable").css("border-spacing",(size-1)/4+"px");
+        $(".match").css("padding",(size-1)/8*3+"px");
+        $(".blue").css("padding",(size-1)/8*3+"px");
+        $(".red").css("padding",(size-1)/8*3+"px");
+        $(".outcome").css("padding",(size-1)/8*3+"px");
+        $("#countdown").css("padding",(size-1)/8*3+"px");
+    }
+});
+$(document).ready(function() {
+    $(window).resize();
+});
+setTimeout('Redirect()',<?php echo $refreshTime * 1000;?>);
+setInterval('count()',1000);
+var dest = new Date(<?php echo $destdate*1000; ?>);
+var flipflop=false;
+
+function Redirect()
+{
+  location.reload(true);
+}
+function count() {
+    var cur = new Date();
+    var diff = (dest - cur) / 1000;
+    if(diff<0) {
+        $("#countdown").text("Countdown: Complete");
+        return;
+    }
+
+    var hours = Math.floor(diff / 3600);
+    var minutes = Math.floor((diff % 3600) / 60);
+    var seconds = Math.floor(diff % 60);
+    if(hours<10) {
+        hours="0"+hours;
+    }
+    if(minutes<10) {
+        minutes="0"+minutes;
+    }
+    if(seconds<10) {
+        seconds="0"+seconds;
+    }
+    $("#countdown").text("Countdown: "+hours+":"+minutes+":"+seconds);
+    if(diff <= 60) {
+        if(flipflop) {
+            $("#countdown").css("background-color","red");
+        } else {
+            $("#countdown").css("background-color","gold");
+        }
+        flipflop = !flipflop;
+    } else if (diff <= 300) {
+        $("#countdown").css("background-color","red");
+    } else if (diff <= 600) {
+        $("#countdown").css("background-color","gold");
+    }
+}
+count();
--- web.js
+++ web.js
@@ -1,18 +1,105 @@
-var express = require('express');
+var express = require("express");
+var http = require("http");
+var cheerio = require("cheerio");
 
 var app = express.createServer(express.logger());
+app.set('views', __dirname + '/views')
+app.set('view options', {
+      layout: false
+});
+app.set('view engine', 'jade')
+app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  var team="254";
-  var ev="casd";
-  var baseurl="http://chiefdelphi.com/forums/frcspy.php?xml=2";
-  var cdurl=baseurl+"&teams="+team+"&events="+ev;
-  var refresh=10;
-  $.get(url,function(xml) {
-  var json = $.xml2json(xml);
-  console.log(json);
-  response.send(json);
-  });
+app.get('/', function(req, res) {
+  var year = req.query.year || "2013"
+  var refresh=parseInt(req.query.refresh) || 10;
+  var team=req.query.team || "254";
+  var ev=req.query.event || "casd";
+  var elims=parseInt(req.query.elims) || 0;
+  http.get({
+            host: "www2.usfirst.org",
+            path: "/"+year+"comp/events/"+ev+"/matchresults.html"
+           },
+           function(response) {
+             var str="";
+             response.on("data", function(chunk) {
+               str += chunk;
+             });
+             response.on('end', function() {
+                 $ = cheerio.load(str);
+                 var tables = $("table tbody");
+                 var table = elims ? tables[3] : tables[2];
+                 var rows = $(table).children();
+                 var data = [];
+                 for(var i=3;i<rows.length;i++) {
+                   var row = rows[i];
+                   var rowdata = $(row).children();
+                   data.push({time: $(rowdata[0]).text(),
+                             match: $(rowdata[1]).text(),
+                             red1: $(rowdata[2]).text(),
+                             red2: $(rowdata[3]).text(),
+                             red3: $(rowdata[4]).text(),
+                             blue1: $(rowdata[5]).text(),
+                             blue2: $(rowdata[6]).text(),
+                             blue3: $(rowdata[7]).text(),
+                             redscore: $(rowdata[8]).text(),
+                             bluescore: $(rowdata[9]).text()});
+                 }
+                 var display = "table class='mainTable'><tr class=matchRow><td class=match>Match</td><td class=red>Red Alliance</td><td class=blue>Blue Alliance</td><td class=outcome>Time / Result</td></tr>";
+                 for(var i=0;i<data.length;i++) {
+                   console.log(i);
+                   var row = data[i];
+                   var potential = [row.red1, row.red2, row.red3, row.blue1, row.blue2, row.blue3];
+                   var spot = potential.indexOf(team);
+                   if (spot > -1) {
+                     var redness = spot < 3 ? "myred" : "";
+                     var blueness = spot > 2 ? "myblue" : "";
+                     display+="<tr class=matchRow><td class=match>"+row.match+"</td><td class='red "+redness+"'><table><tr>";
+                     for(var j=0;j<3;j++) {
+                       display+="<td class='oneTeam ";
+                       if (spot == j) {
+                         display += "myTeam";
+                       }
+                       display+="'>"+row["red"+(j+1)]+"</td>";
+                     }
+                     display+="</tr></table></td><td class='blue "+blueness+"'><table><tr>";
+                     for(var j=0;j<3;j++) {
+                       display+="<td class='oneTeam ";
+                       if (spot == j+3) {
+                         display += "myTeam";
+                       }
+                       display+="'>"+row["blue"+(j+1)]+"</td>";
+                     }
+                     display+="</tr></table></td><td class=outcome>";
+
+                     var first = spot < 3 ? "myTeam" : "";
+                     var second = spot > 2 ? "myTeam" : "";
+
+                     if (row.redscore != "" && row.bluescore != "") {
+                       var outcome = "";
+                       if (row.redscore == row.bluescore) {
+                         outcome = "T";
+                       }
+                       else if ((spot < 3 && parseInt(row.redscore) > parseInt(row.bluescore)) ||
+                                (spot > 2 && parseInt(row.redscore) < parseInt(row.bluescore))) {
+                         outcome = "W"
+                       }
+                       else {
+                         outcome = "L";
+                       }
+                       display+="<table><tr><td class='oneTeam "+first+"'>"+row.redscore+"</td><td class='oneTeam "+second+"'>"+row.bluescore+"</td><td class=oneTeam>"+outcome+"</td></tr></table>";
+                     }
+                     else {
+                       display += row.time;
+                     }
+                     display+= "</td></tr>";
+                   }
+                 }
+                 display+="</table";
+                 res.render('display', {disp: display});
+             });
+           }
+  );
 });
 
 var port = process.env.PORT || 5000;
